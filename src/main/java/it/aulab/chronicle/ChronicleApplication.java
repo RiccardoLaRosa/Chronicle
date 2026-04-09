845327e468891b8f7208bd2bd42a7927e0dc59d2
Riccardo La Rosa <riccardofedericolarosa@gmail.com>
2026-04-09 10:54:25 +0200

    scaffolding del progetto, cartella sql, creato database

---
+++ src/main/java/it/aulab/chronicle/ChronicleApplication.java
@@ -0,0 +1,13 @@
+package it.aulab.chronicle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronicleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChronicleApplication.class, args);
+	}
+
+}
