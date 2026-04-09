5b5b74d92d8476475cf45ea546518c6ef88cf062
Riccardo La Rosa <riccardofedericolarosa@gmail.com>
2026-04-09 17:12:38 +0200

    Creati DtoUser,  RepositoryUser, ServiceUser, ControllerUser

---
+++ src/main/java/it/aulab/chronicle/controllers/UserController.java
@@ -0,0 +1,21 @@
+package it.aulab.chronicle.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import it.aulab.chronicle.services.UserService;
+
+@Controller
+public class UserController {
+    
+    @Autowired
+    private UserService userService;
+
+    /* Rotta Home */
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+    
+}
---
+++ src/main/java/it/aulab/chronicle/services/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package it.aulab.chronicle.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import it.aulab.chronicle.dtos.UserDto;
+import it.aulab.chronicle.models.User;
+import it.aulab.chronicle.repositories.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Override
+    public void saveUser(UserDto userDto, RedirectAttributes redirectAttributes, HttpServletRequest request, HttpServletResponse response) {
+        User user = new User();
+        user.setUsername(userDto.getFirstname() + " " + userDto.getLastname());
+        user.setEmail(userDto.getEmail());
+        user.setPassword(passwordEncoder().encode(userDto.getPassword()));
+    }
+
+    @Override
+    public User findUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+}
